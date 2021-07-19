19f1433094df1e74861a631fe2cf9fe07e19062f
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-07-20 01:01:48 +0300

    Create jobAdvertisement Right Rail Content

---
+++ hrms-project/src/pages/JobAdvertisement/RightRailContent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function RightRailContent() {
+    return (
+        <div>
+            <button className="ui fluid button">Profile Page</button>
+        </div>
+    )
+}
